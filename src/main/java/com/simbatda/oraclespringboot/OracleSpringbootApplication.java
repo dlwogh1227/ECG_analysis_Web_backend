868b625df625532dbd9472ed2dceab1e3bff4e7a
KDT-25 <dlwogh1227@naver.com>
2025-07-09 10:02:00 +0900

    first commit

---
+++ src/main/java/com/simbatda/oraclespringboot/OracleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.simbatda.oraclespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleSpringbootApplication.class, args);
+    }
+
+}
